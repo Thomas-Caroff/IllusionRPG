56cb73b8e009102cfb7670e61140c215e0ac4753
thomas.caroff <[thomasc97@hotmail.fr]>
2023-11-25 21:08:51 +0100

    [INIT] Created first models and added .gitignore

--- illusion/webapp/models.py
+++ illusion/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    uuid = models.UUIDField()
+    username = models.CharField(max_length=200)
+    pseudo = models.CharField(max_length=200)
+    party = models.JSONField()
+
+    def __str__():
+        return self.username
+
+
+class Character(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    character_name = models.CharField(max_length=200)
+    level = models.IntegerField(default=0)
+
+    def __str__():
+        return self.character_name
