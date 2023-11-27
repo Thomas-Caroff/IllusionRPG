b9b27c0f87e877261bb64ebe22595357d23dd3c4
thomas.caroff <[thomasc97@hotmail.fr]>
2023-11-27 17:42:59 +0100

    [INIT+DEV] Guid on all models, starting to create views

--- illusion/webapp/models.py
+++ illusion/webapp/models.py
@@ -1,19 +1,22 @@
 from django.db import models
+import uuid
 
-class User(models.Model):
-    uuid = models.UUIDField()
+class GUIDModel(models.Model):
+    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class User(GUIDModel):
     username = models.CharField(max_length=200)
     pseudo = models.CharField(max_length=200)
     party = models.JSONField()
 
-    def __str__():
+    def __str__(self):
         return self.username
 
 
-class Character(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Character(GUIDModel):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     character_name = models.CharField(max_length=200)
     level = models.IntegerField(default=0)
 
-    def __str__():
+    def __str__(self):
         return self.character_name
--- illusion/webapp/views.py
+++ illusion/webapp/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the main page index.")
+
+def character_detail(request, character_id):
+    return HttpResponse(f"You're searching for character {character_id}")
+
+def user_profile(request, user_id):
+    return HttpResponse(f"You're searching for user {user_id}")
