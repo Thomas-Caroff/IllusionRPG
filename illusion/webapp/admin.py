6ea99cfc8512427e0a4ceb86f47824983f33f212
thomas.caroff <[thomasc97@hotmail.fr]>
2023-11-30 00:27:44 +0100

    [DEV] Adding more details to models, migrated

--- illusion/webapp/admin.py
+++ illusion/webapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User, Character
+from .models import User, Character, Campaign, Party
 
 admin.site.register(User)
 admin.site.register(Character)
+admin.site.register(Campaign)
+admin.site.register(Party)
--- illusion/webapp/models.py
+++ illusion/webapp/models.py
@@ -2,27 +2,39 @@ from django.db import models
 import utils.customUUID as guid
 
 class GUIDModel(models.Model):
-    id = models.CharField(primary_key=True, max_length=32)
+    id = models.CharField(primary_key=True, max_length=32, default=guid.custom_id)
 
 class User(GUIDModel):
     ##Infos
-    username = models.CharField(max_length=200)
-    pseudo = models.CharField(max_length=200)
-    party_list = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
+    pseudo = models.CharField(default="", blank=True, max_length=200, unique=True)
+    party_list = models.CharField(blank=True, max_length=200)
 
     def __str__(self):
         return self.username
+
+
+class Campaign(GUIDModel):
+    campaign_name = models.CharField(default="", max_length=100, unique=True)
+    description = models.CharField(blank=True, default="", max_length=5000)
+
+
+class Party(GUIDModel):
+    party_name = models.CharField(default="", max_length=100)
+    quadgram = models.CharField(default="", max_length=4, unique=True)
+    campaign_id = models.ForeignKey(Campaign, on_delete=models.CASCADE)
 
 
 class Character(GUIDModel):
     ##Global
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     character_name = models.CharField(default="", max_length=200)
-    level = models.IntegerField(default=0)
-    description = models.CharField(default="", max_length=5000)
+    description = models.CharField(blank=True, default="", max_length=5000)
+    party_id = models.ForeignKey(Party, null=True, blank=True, on_delete=models.SET_NULL)
 
     ##Accessories
-    items = models.CharField(default="", max_length=500)
+    items = models.CharField(blank=True, default="", max_length=500)
+    level = models.IntegerField(default=0)
 
     ##Stats
     strength = models.IntegerField(default=10)
@@ -34,16 +46,3 @@ class Character(GUIDModel):
 
     def __str__(self):
         return self.character_name
-
-
-
-class Campaign(GUIDModel):
-    campaign_name = models.CharField(default="", max_length=100)
-    description = models.CharField(default="", max_length=5000)
-
-
-class Party(GUIDModel):
-    party_name = models.CharField(default="", max_length=100)
-    quadgram = models.CharField(default="", max_length=4)
-    party_character = models.CharField(default="", max_length=200)
-    campaign_id = models.ForeignKey(Campaign, on_delete=models.CASCADE)
