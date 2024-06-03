15b58636b060ace6067a784970c218ceb0579a8b
thomas.caroff <[thomasc97@hotmail.fr]>
2024-06-04 00:52:48 +0200

    [CONFIG] Updated CORS and ALLOWED_HOSTS config to connect API and React

--- illusion/illusion/settings.py
+++ illusion/illusion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t0b^l_1$q7pll0pl$x&4-5ym+$jws(s$5dyn%t+yh-@5l6_(bj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,7 +52,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
